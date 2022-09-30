c2f30a4474ac0805b616355ad2d17d6544aa0fe1
joab <pbnjoab@gmail.com>
2022-09-30 01:00:19 -0400

    Finish the game. Now printing correctly and tracking scores correctly

--- rps.js
+++ rps.js
@@ -3,18 +3,16 @@ function getComputerChoice(){
     const cpuChoice = choices[Math.floor(Math.random() * choices.length)]; // randomizes a choice in the array
     return cpuChoice;
 }
-let cpu = getComputerChoice();
-console.log(cpu); // checking computer choice for debugging
 
 
 function playerChoice(){ //passed on to playGame function for user input
     let choice = prompt("Choose your weapon: Rock, Paper, Scissors").toLowerCase(); // makes everything lowercase
     choice = choice[0].toUpperCase() + choice.slice(1); // capitalizes first letter of user input
-    console.log(choice); // to check user input case
+    // console.log(choice); // to check user input case
     return choice;
 }
 
-function playGame(playerSelection = playerChoice(), computerSelection = cpu){
+function playGame(playerSelection = playerChoice(), computerSelection = getComputerChoice()){
     
     if (playerSelection === computerSelection) {
         return ("Tied");
@@ -23,20 +21,34 @@ function playGame(playerSelection = playerChoice(), computerSelection = cpu){
         playerSelection === "Paper" && computerSelection === "Rock" ||
         playerSelection === "Scissors" && computerSelection === "Paper"){
         return (`You win! ${playerSelection} beats ${computerSelection}`);
-    }
-    else {
+    } else {
         return(`You lose! ${computerSelection} beats ${playerSelection}`);
     }
 }
 
-function game() {
+function game() { // needs output fix!!
     let playerScore = 0;
     let computerScore = 0;
-    let newGame;
 
     for (let i = 0; i < 5; i++) {
-        newGame = playGame();
-        console.log(newGame)
+        const outcome =  playGame();
+        console.log(outcome);
+        let outcomeStr = String(outcome);
+        if (outcomeStr.includes("win")) {
+            playerScore += 1;
+            console.log(`player score: ${playerScore} | computer score ${computerScore}`);
+        }
+        else if (outcomeStr.includes("lose")) {
+            computerScore += 1;
+            console.log(`player score: ${playerScore} | computer score ${computerScore}`);
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log("You win the game!");
+    } else if (playerScore < computerScore) {
+        console.log("You lost the game!");
+    } else {
+        console.log("Tied game.")
     }
 }
 game();
